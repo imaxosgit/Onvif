908c6e1b8d6660063d98304282ace5ead7b6ae4c
imaxosgit <mmaxosipov@gmail.com>
2018-03-31 03:26:28 +0300

    ptz

---
+++ myptz.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from onvif import ONVIFCamera
+from time import sleep
+
+print 'INIT'
+
+mycam = ONVIFCamera('192.168.13.12', 80, 'admin', 'Supervisor', '/etc/onvif/wsdl/')
+
+print 'Connected to ONVIF camera'
+media = mycam.create_media_service()
+print 'Created media service object'
+media_profile = media.GetProfiles()[0]
+token = media_profile._token
+
+#image = mycam.create_imaging_service()
+#reqi = image.create_type('SetImagingSettings')
+#reqi.VideoSourceToken = media_profile.VideoSourceConfiguration.SourceToken
+#reqi.Brightness = 50
+#image.SetImagingSettings(reqi)
+
+
+print 'Creating PTZ object'
+ptz = mycam.create_ptz_service()
+
+request = ptz.create_type('GetServiceCapabilities')
+Service_Capabilities = ptz.GetServiceCapabilities(request)
+print 'PTZ service capabilities:'
+print Service_Capabilities
+
+status = ptz.GetStatus({'ProfileToken':token})
+print 'PTZ status:'
+print status
+print 'Pan position:', status.Position.PanTilt._x
+print 'Tilt position:', status.Position.PanTilt._y
+print 'Zoom position:', status.Position.Zoom._x
+
+reqconmv = ptz.create_type('ContinuousMove')
+reqconmv.ProfileToken = media_profile._token
+
+requests = ptz.create_type('Stop')
+requests.ProfileToken = media_profile._token
+
+def stop():
+	requests.PanTilt=True
+	requests.Zoom=True
+	ptz.Stop(requests)
+	print 'Stopped'
+
+
+print
+stop()
+
+def move(pan, tilt, velocity):
+        print 'Start moving'
+        reqconmv.Velocity.PanTilt._x = velocity
+	reqconmv.Velocity.PanTilt._y = velocity
+        token = media_profile._token
+        status = ptz.GetStatus({'ProfileToken':token})
+	sig = 0.01
+        print 'Pan position:', status.Position.PanTilt._x
+        print 'Tilt position:', status.Position.PanTilt._y
+        while abs(pan - status.Position.PanTilt._x) > sig  and abs(tilt - status.Position.PanTilt._y) > sig:
+            if pan > status.Position.PanTilt._x:
+                reqconmv.Velocity.PanTilt._x = velocity
+                print 'going x'
+            else:
+                reqconmv.Velocity.PanTilt._x = -1*velocity
+                print 'going -x'
+            if tilt > status.Position.PanTilt._y:
+                reqconmv.Velocity.PanTilt._y = velocity
+                print 'going y'
+            else:
+                reqconmv.Velocity.PanTilt._y = -1*velocity
+                print 'going -y'
+            if abs(pan - status.Position.PanTilt._x) < sig:
+                reqconmv.Velocity.PanTilt._x = 0
+                break
+            if abs(tilt - status.Position.PanTilt._y) < sig:
+                reqconmv.Velocity.PanTilt._y = 0
+                break
+            ptz.ContinuousMove(reqconmv)
+            status = ptz.GetStatus({'ProfileToken':token})
+            print 'Pan position:', status.Position.PanTilt._x
+            print 'Tilt position:', status.Position.PanTilt._y
+            print '-----------------------------------------------'
+	stop()
+
+def zoom(velocity, scope):
+	print 'Zooming'
+	reqconmv.Velocity.PanTilt._x=0
+	reqconmv.Velocity.PanTilt._y=0
+	reqconmv.Velocity.Zoom._x = velocity
+	while (status.Position.Zoom._x < scope):
+		ptz.ContinuousMove(reqconmv)
+#		print 'Zoom: ', status.Position.Zoom._x
+	stop()
+
+
+
+
+move(-0.5,-0.5, 0.4)
+
+sleep(2)
+
+zoom(0.1, 0.5)
